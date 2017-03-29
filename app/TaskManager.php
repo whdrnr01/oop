689ea872c05d5e41132c7f024b4bbebb58e7767d
cOOkIE <whdrnr01@gmail.com>
2017-03-29 21:26:29 +0900

    OOP #7 typehint

--- app/TaskManager.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Todo;
-
-
-class TaskManager 
-{
-	
-}
--- index.php
+++ index.php
@@ -1,13 +1,57 @@
 <?php
-// require 'Models/User.php';
-use Todo\Models\Task;
-use Todo\Models\User;
 
-require_once 'vendor/autoload.php';
+interface ItemInterface
+{
+	public function setCost(float $cost);
+	public function getCost();
+}
 
-$task = new Task;
-$user = new User;
 
-var_dump($user);
-// $user = new User;
-// var_dump($user->username);
+class Item implements ItemInterface
+{
+	protected $cost = 0;
+
+	public function setCost(float $cost)
+	{
+		$this->cost = $cost;
+	}
+
+	public function getCost()
+	{
+		return $this->cost;
+	}
+}
+
+class Cart 
+{
+	protected $items = [];
+
+	public function add(ItemInterface $item)
+	{
+		$this->items[] = $item;
+	}
+
+	public function total()
+	{
+		$total = 0;
+
+		foreach ($this->items as $item) {
+			$total += $item->getCost();
+		}
+
+		return $total;
+	}
+}
+
+
+$item1 = new Item;
+$item1->setCost(5);
+
+$item2 = new Item;
+$item2->setCost(20.5);
+
+$cart = new Cart;
+$cart->add($item1);
+$cart->add($item2);
+
+echo $cart->total();
